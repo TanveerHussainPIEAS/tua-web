fd3e3cf9746cb4b57dcd12bf47f4b773262bbd27
Tanveer Hussain <thussain@cinnova.com>
2023-02-21 14:16:06 +0500

    ...

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,17 +5,23 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, }  , 
   { path: 'about', component: AboutComponent, },
   { path: 'contact', component: ContactComponent, },
+  { path: 'login', component: LogInComponent, },
+  { path: 'register', component: RegisterComponent, },
 ];
 @NgModule({
   declarations: [
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LogInComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
