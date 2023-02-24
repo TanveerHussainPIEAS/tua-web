1a8f8f96c554950cece21af6d8e81dc8af07c834
Tanveer Hussain <thussain@cinnova.com>
2023-02-24 17:17:24 +0500

    | User Permissions Added | TH |

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -11,5 +11,25 @@ export interface LoginModel {
     userName: string;
     name: string;
     token: string;
+    userPermission: UserPermission;
   }
 
+  export interface UserPermission {
+    id:number;
+    userId: number;
+    canAddProduct: boolean; 
+    canEditProduct: boolean;
+    canViewProduct: boolean;
+    canDeleteProduct: boolean;
+    canAddOrder: boolean;
+    canEditOrder: boolean;
+    canViewOrder: boolean;
+    canDeleteOrder: boolean;
+    canAddUser: boolean;
+    canEditUser: boolean;
+    canViewUser: boolean;
+    canDeleteUser: boolean;   
+  }
+
+
+ 
